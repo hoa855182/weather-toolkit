3a085ef636713cd3cffa8bcc96fe35b5696f70c0
hoa855182 <hoa855182@gmail.com>
2022-05-27 15:10:10 +0700

    redux toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -3,22 +3,19 @@ import thunk from 'redux-thunk';
 
 // import weatherReducer from '../Redux/WeatherReducer'
 import weatherReducer from '../Redux/weatherSlice'
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './rootSaga';
 
 
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
+
+
 
 export const store = configureStore({
   reducer: {
     weather: weatherReducer,
     
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleware),
+  
 });
 
-sagaMiddleware.run(rootSaga);
+
 
 
